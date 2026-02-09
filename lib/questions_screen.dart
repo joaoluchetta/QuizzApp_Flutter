b515ea486f865e1e3a0ff8cba6e544de8b73ae96
joaoluchetta <joaoluchetta.dev@gmail.com>
2026-02-09 09:48:09 -0300

    Passing Data via Functions Across Widgets

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -4,7 +4,9 @@ import 'package:quizz_app/data/questions.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -15,7 +17,10 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
 
-  void answerQuestion() {
+  void answerQuestion(String selectedAnswers) {
+    //widget. is used to access the properties of the widget class from the state class
+    widget.onSelectAnswer(selectedAnswers);
+     
     setState(() {
       currentQuestionIndex++;
     });
@@ -43,8 +48,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            ...currentQuestion.getShuffleAnswers().map((item) {
-              return AnswerButton(answerText: item, onTap: answerQuestion);
+            ...currentQuestion.getShuffleAnswers().map((answer) {
+              return AnswerButton(answerText: answer,
+               onTap: (){
+                answerQuestion(answer);
+              },
+              );
             }),
           ],
         ),
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_app/questions_screen.dart';
 import 'package:quizz_app/start_screen.dart';
+import 'package:quizz_app/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -12,22 +13,44 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizzState extends State<Quiz> {
-  Widget? activeScreen;
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+  //Widget? activeScreen;
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+      //activeScreen = const QuestionsScreen();
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
+  }
 
   @override
   void initState() {
     activeScreen = StartScreen(switchScreen);
     super.initState();
   }
- 
-  void switchScreen() {
-    setState(() {
-      activeScreen = const QuestionsScreen();
-    });
-  }
 
   @override
-  Widget build(context) {
+  Widget build(context) {    
+    Widget screenWidget = StartScreen(switchScreen);
+
+  if(activeScreen == 'questions-screen'){
+    screenWidget = const QuestionsScreen(
+      onSelectAnswer: chooseAnswer
+    );
+  }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
